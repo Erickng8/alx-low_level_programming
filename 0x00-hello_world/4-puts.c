588a274caef00eeaf92e18dab1bec50e93bf7f59
Erickng8 <ngugierick519@gmail.com>
2023-02-17 04:19:52 -0800

    use of puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 after printing
+ */
+
+int main(void)
+{
+	puts('\'"Programming is like building a multilingual puzzle);
+	Return (0);
+}
