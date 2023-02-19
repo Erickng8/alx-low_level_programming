3ec035c7095fef4f8165a19ee4de0b29ac094e12
Erickng8 <ngugierick519@gmail.com>
2023-02-19 11:45:50 -0800

    lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase in reverse
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char val;
+
+	for (val = 'z'; val <= 'a'; val--)
+		putchar(val);
+	{
+		putchar('\n');
+	}
+	return (0);
+}
