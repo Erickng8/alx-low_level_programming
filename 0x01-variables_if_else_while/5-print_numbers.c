c291cc147a6f015cf4575bfc28a8ec2b2c14b83b
Erickng8 <ngugierick519@gmail.com>
2023-02-19 11:27:50 -0800

    numbers between 0 and 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers with single digit from 0 to 10
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n < '10'; n++)
+		putchar(n);
+		{
+			putchar('\n');
+		}
+	return (0);
+}
