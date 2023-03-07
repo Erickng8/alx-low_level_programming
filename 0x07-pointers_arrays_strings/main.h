f59a6360263329cdaa6bd2ded7790ccc5145c2d9
Erickng8 <ngugierick519@gmail.com>
2023-03-07 03:26:27 -0800

    ata sijui

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H                                                                                             
+#ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
@@ -9,7 +9,7 @@ char *_memset(char *s, char b, unsigned int n);
 
 char *_memcpy(char *dest, char *src, unsigned int n);
 
-char *_strchr(char *s,char c);
+char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
 
